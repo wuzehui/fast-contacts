0770c838ec2c16f91e02547b9e74efdd60ad5b5f
paris tao <paristao1989@gmail.com>
2014-07-19 18:57:55 +0800

    spring security

--- src/main/java/demo/controller/DemoController.java
+++ src/main/java/demo/controller/DemoController.java
@@ -2,8 +2,10 @@ package demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
@@ -13,4 +15,53 @@ public class DemoController {
 		System.out.println("******* here ******");
 		return "demo/index";
 	}
+	
+	@RequestMapping(value="/",method = RequestMethod.GET)
+	public String index(Model model){
+		System.out.println("******* here ******");
+		return "login";
+	}
+	
+	@RequestMapping(value = "admin",method = RequestMethod.GET)
+	public String admin(Model model){
+		System.out.println("********** admin page **********");
+		return "demo/adminpage";
+	}
+	
+	@RequestMapping(value = "common",method = RequestMethod.GET)
+	public String common(Model model){
+		System.out.println("********** common page **********");
+		return "demo/commonpage";
+	}
+	
+	 /** 
+     * 指向登录页面 
+     */  
+    @RequestMapping(value = "auth/login", method = RequestMethod.GET)  
+    public String getLoginPage(  
+            @RequestParam(value = "error", required = false) boolean error,  
+            ModelMap model) {  
+  
+        if (error == true) {  
+            // Assign an error message  
+            model.put("error",  
+                    "You have entered an invalid username or password!");  
+        } else {  
+            model.put("error", "");  
+        }  
+        return "loginpage";  
+  
+    }  
+  
+    /** 
+     * 指定无访问额权限页面 
+     *  
+     * @return 
+     */  
+    @RequestMapping(value = "auth/denied", method = RequestMethod.GET)  
+    public String getDeniedPage() {  
+  
+        return "deniedpage";  
+  
+    }  
 }
