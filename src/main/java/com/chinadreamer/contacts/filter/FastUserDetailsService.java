95eeb1bd6c15d737f68b925ec48bc7284028e85f
Sos10086tsj <paristao1989@gmail.com>
2014-08-04 13:50:50 +0800

    spring security 拦截demo

--- src/main/java/com/chinadreamer/contacts/filter/FastUserDetailsService.java
+++ src/main/java/com/chinadreamer/contacts/filter/FastUserDetailsService.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService{
+public class FastUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String arg0)
--- src/main/java/com/chinadreamer/contacts/user/controller/UserController.java
+++ src/main/java/com/chinadreamer/contacts/user/controller/UserController.java
@@ -10,12 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("fastcontacts")
 public class UserController {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String index(Model model){
-		return "login";
-	}
-	
+
 	@RequestMapping(value="login",method=RequestMethod.POST)
 	public String login(Model model,HttpServletRequest request){
 		System.out.println(request.getParameterNames());
